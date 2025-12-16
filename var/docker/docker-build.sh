008d12f79606ec51ea6e2653ed504f7fc90beaa7
vivek-inloops <vivek@inloops.in>
2025-12-16 11:53:09 +0530

    changes to workflow

--- var/docker/docker-build.sh
+++ var/docker/docker-build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-set -o xtrace
-
-docker rmi localhost/postiz || true
-docker build --target dist -t localhost/postiz -f Dockerfile.dev .
-docker build --target devcontainer -t localhost/postiz-devcontainer -f Dockerfile.dev .
+# Build the docker image locally
+docker build -f Dockerfile.dev -t postiz-app:latest .
